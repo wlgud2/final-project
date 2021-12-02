7cc8b9c687a697a3e5bc61221a3ffc1d86716670
wlgud2 <akddnfsla@gmail.com>
2021-12-02 17:50:12 +0900

    update 진행상황

--- database2.sql
+++ database2.sql
@@ -1,6 +1,12 @@
 insert into zagook_member(id, password, mname, email,job, mdate)
 values('user1', 1234, '홍길동', 'user1@com', '학생', sysdate);
 /*게시글, 태그 생성*/
+insert into zagook_contents(contentsno, id, filename, contents,
+rdate, privacy, x_site, y_site)
+values((select nvl(max(contentsno),0)+1
+from zagook_contents),
+'user_3', 'paris.jpg', '다른 지역 게시글321', sysdate, 1,35.83709842555726, 128.6979583564257);
+
 insert	into zagook_contents(contentsno, ID, FILENAME, contents,
 rdate, privacy)
 values((select nvl(max(contentsno),0)+1
@@ -18,15 +24,17 @@ from zagook_tag), '없는 태그');
 
 insert into zagook_posttag(contentsno, tag_id)
 values ((select max(contentsno)
-from zagook_contents), (select tag_id from zagook_tag where tag='새 태그'));
+from zagook_contents), (select tag_id from zagook_tag where tag='nice'));
+
+insert into zagook_posttag(contentsno, tag_id)
+values (320, (select tag_id from zagook_tag where tag='nice'));
 
 alter table zagook_tag add constraint uniquekey unique (tag);
 
 /*수정*/
 update zagook_contents
-set contents='update contents',
-privacy=2
-where contentsno = 302;
+set filename='eiffel_tower.jpg'
+where contentsno =321;
 --기존 태그 삭제
 delete from zagook_posttag
 where contentsno=310;
@@ -42,18 +50,35 @@ where contentsno=307;
 
 /*삭제*/
 delete from zagook_posttag
-where contentsno>307;
+where contentsno=314;
 
 delete from zagook_tag
-where tag_id=17;
+where tag_id>20;
 
 delete from zagook_contents
-where contentsno>307;
+where contentsno>312;
 
-/*목록*/
+/*구 디테일*/
 select c.contentsno, c.contents, c.privacy, m.mname, c.filename, t.tag, c.rdate, c.likecnt
 from zagook_contents c, zagook_tag t, zagook_posttag p, zagook_member m
 where m.id=c.id
 and c.contentsno=p.contentsno
 and t.tag_id=p.tag_id
-and t.tag_id=11;
+and c.contentsno=308;
+/*디테일 수정*/
+select c.contentsno, c.contents, c.privacy, m.mname, c.filename, c.rdate, c.likecnt,
+'#'||(select listagg(t.tag,' #') within group(order by p.contentsno)
+from zagook_tag t, zagook_posttag p
+where EXISTS(
+    select p.tag_id from zagook_contents c, zagook_posttag p
+    where c.contentsno=p.contentsno
+)
+and t.tag_id=p.tag_id
+and p.contentsno=313
+)as tag
+from zagook_contents c, zagook_member m
+where m.id=c.id
+and c.contentsno=313;
+
+/*태그 공백제거*/
+update zagook_tag set tag =(replace(tag,' ','_'));
